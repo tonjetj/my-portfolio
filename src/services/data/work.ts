297236e442d2e3081b804b49c4f410733c78f4d3
tonjetj <tontotjen@gmail.com>
2024-11-23 01:50:21 +0100

    adding work data

---
+++ src/services/data/work.ts
@@ -0,0 +1,69 @@
+export type Work = {
+  id: number;
+  title: string;
+  description: string;
+  img?: string;
+  demoUrl: string;
+  githubUrl: string;
+  figmaUrlDesktop: string;
+  figmaUrlMobile: string;
+  languages: string[];
+  frameworks: string[];
+  technologies: string[];
+};
+
+export const WorkList: Work[] = [
+  {
+    id: 0,
+    title: "Findings",
+    description:
+      "Findings is a website for an online NFT auction, where you can purchase and sell unique NFTs. The site is minimalistic with a dark yet vibrant color palette. Built with HTML, JavaScript, SASS and Bootstrap v5.2. The site is deployed on Netlify.",
+    img: "",
+    demoUrl: "",
+    githubUrl: "",
+    figmaUrlDesktop: "",
+    figmaUrlMobile: "",
+    languages: ["HTML", "JavaScript"],
+    frameworks: ["SASS", "Bootstrap v5.2"],
+    technologies: [""],
+  },
+  {
+    id: 1,
+    title: "",
+    description: "",
+    img: "",
+    demoUrl: "",
+    githubUrl: "",
+    figmaUrlDesktop: "",
+    figmaUrlMobile: "",
+    languages: ["HTML", "JavaScript"],
+    frameworks: ["SASS", "Bootstrap v5.2"],
+    technologies: [""],
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    img: "",
+    demoUrl: "",
+    githubUrl: "",
+    figmaUrlDesktop: "",
+    figmaUrlMobile: "",
+    languages: ["HTML", "JavaScript"],
+    frameworks: ["SASS", "Bootstrap v5.2"],
+    technologies: [""],
+  },
+  {
+    id: 3,
+    title: "",
+    description: "",
+    img: "",
+    demoUrl: "",
+    githubUrl: "",
+    figmaUrlDesktop: "",
+    figmaUrlMobile: "",
+    languages: ["HTML", "JavaScript"],
+    frameworks: ["SASS", "Bootstrap v5.2"],
+    technologies: [""],
+  },
+];
